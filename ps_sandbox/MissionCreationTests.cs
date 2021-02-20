f48a48d57c798d7dd8c3fba956cb873ef875550b
Olga <49266934+olgakos@users.noreply.github.com>
2021-02-20 15:21:33 +0300

    Тест Создать Запрос на Командировку

---
+++ ps_sandbox/MissionCreationTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace PsSandbox
+{
+    [TestFixture]
+    public class MissionMiniTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://62.152.34.179:8080/Auth";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void MissionMiniCreationTest()
+        {
+            //OpenHomePage
+            driver.Navigate().GoToUrl("BaseURL");
+            //Login
+            driver.FindElement(By.Name("login")).Click();
+            driver.FindElement(By.Name("login")).Clear();
+            driver.FindElement(By.Name("login")).SendKeys("Stranger");
+            driver.FindElement(By.Name("password")).Click();
+            driver.FindElement(By.Name("password")).Clear();
+            driver.FindElement(By.Name("password")).SendKeys("66PojexoIEB0");
+            driver.FindElement(By.Name("valid_auth")).Click(); // войти
+            //ОЖИДАНИЕ!
+            Thread.Sleep(2000);
+
+            //GoToReesterMissions
+            driver.Navigate().GoToUrl(baseURL + "/Missions/");
+            
+            //InitNewItemCreation (кнопка "создать новый")
+            driver.FindElement(By.Id("ButtonAddNewElem")).Click();
+
+            //ждем кнопку со стрелкой < т.е. разворачиваем картчоку на весь экран (чтобы скрыть с монитора лишние поля с похожими именами)
+            driver.FindElement(By.Id("button_hidden_datatables")).Click();
+
+            //начинаем заполняить обязательные поля:
+            driver.FindElement(By.Id("select2-StaffID_NEW_NEW-container")).Click(); //нашли поле "сотрудник"
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys("TestMan1");//печатаем (полностью) имя из выпадающего списка
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys(Keys.Enter);//подтвержение
+            driver.FindElement(By.Id("select2-Firm_NEW_NEW-container")).Click(); //нашли поле "место работы"
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys("Компания 2");//печатаем (полностью) имя из выпадающего списка
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys(Keys.Enter);// подтвержение
+            driver.FindElement(By.Id("select2-Filial_NEW_NEW-container")).Click(); //нашли поле "Филиал"
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys("Москва");//печатаем (полностью) имя из выпадающего списка
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys(Keys.Enter);// подтвержение
+            driver.FindElement(By.Id("Purpose_NEW")).Click(); //нашли поле "цель поездки"
+            driver.FindElement(By.Id("Purpose_NEW")).SendKeys("Цель"); // печатем короткий прозвольынй текст
+            driver.FindElement(By.Id("MissionPlace_NEW")).Click(); //нашли поле "место комантировки"
+            driver.FindElement(By.Id("MissionPlace_NEW")).SendKeys("Тверь"); // печатем короткий прозвольынй текст
+            driver.FindElement(By.Id("StartMisDate_NEW")).Click(); //нашли поле "дата начала" ВИДЖЕТ
+            //driver.FindElement(By.LinkText("28")).Click();
+            driver.FindElement(By.LinkText("Сегодня")).Click(); //кнопка сегодня или иное, 
+            driver.FindElement(By.Id("FinishMisDate_NEW")).Click(); //нашли поле "дата окончания" ВИДЖЕТ
+            driver.FindElement(By.LinkText("28")).Click(); //дата, наприемер 28 число
+            driver.FindElement(By.Id("select2-MissionPurpose_NEW_NEW-container")).Click(); //нашли поле "Назначение командировки"
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys("Командировка");//печатаем (полностью) имя из выпадающего списка
+            driver.FindElement(By.XPath("(//input[@type='search'])[2]")).SendKeys(Keys.Enter);
+            driver.FindElement(By.Id("WantResult_NEW")).Click(); //нашли поле "Ожидаемый результат"
+            driver.FindElement(By.Id("WantResult_NEW")).SendKeys("Ожидаемый"); // печатем короткий прозвольынй текст
+            driver.FindElement(By.Id("FinalResult_NEW")).Click(); //нашли поле "Полученный результат"
+            driver.FindElement(By.Id("FinalResult_NEW")).SendKeys("Полученный");// печатем короткий прозвольынй текст
+
+            //нашли-нажали кнопку "Сохранить карточку"
+            driver.FindElement(By.Id("panel_edit_button_action_save")).Click();
+
+            //нашли-нажали кнопку "Разлогиниться"
+            driver.FindElement(By.XPath("//button[@onclick='logout();']")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ ps_sandbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ps_sandbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ps_sandbox")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c4c9222-0288-419f-93ea-ed4f39511f49")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
